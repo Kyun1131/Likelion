ccace18f3f21b6b38c7fa961b9525a0d30a0d61b
Kyun1131 <kyunkyun.1131@gmail.com>
2025-02-21 10:56:54 +0900

    LikeLion3
    LikeLion3

---
+++ LikeLion4-1/LikeLion4-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100; // hp : 100
+            double att = 56.7; // att : 56.7
+            string name = "??"; // 캐릭터 이름 : ??
+            char grade = 'S'; // 등급 : S
+
+            Console.WriteLine("캐릭터");
+            Console.WriteLine("캐릭터 이름 : "+name);
+            Console.WriteLine("등급 : "+grade);
+            Console.WriteLine("hp : "+hp);
+            Console.WriteLine("att : "+att);
+
+
+        }
+    }
+}
