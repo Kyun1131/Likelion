c39b13e4b74935cddbd9449d930495ad1952fc50
Kyun1131 <kyunkyun.1131@gmail.com>
2025-02-21 13:01:03 +0900

    LikeLion6
    LikeLion6

---
+++ LikeLion5-2/LikeLion5-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 같은 데이터 타입의 변수를 쉼표로 구분해 한 번에 선언
+            int x = 10, y = 20, z = 30; // 정수형 변수 x, y, z를 선언하고 초기화
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
